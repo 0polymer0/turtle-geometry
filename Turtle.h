9417badf7c3e70b8f4113fc7e273ce9eea27c5bc
0polymer0 <ajkreitzberg@gmail.com>
2013-11-14 13:45:50 -0800

    cleaned up turtle.h a little.

--- Turtle.h
+++ Turtle.h
@@ -14,57 +14,57 @@ namespace Turt {
 
 class Turtle{
 public:
-	Turtle(
-	  Canvas& can, 
-	  point init_position,
-      Degrees init_angle = 0.0 ) :
+  Turtle(
+    Canvas& can, 
+    point init_position,
+    Degrees init_angle = 0.0 ) :
+  
+    canvas{ can },
+    position(init_position),
+    Angle{init_angle},	
+  
+    turtle_image{ "TurtleAlpha.png" }, //TODO: Magic literal
+    bPenDown{ true }
+  {}
 
-	  canvas{ can },
-	  position(init_position),
-	  Angle{init_angle},	
+  void draw(Graphics& graphics) const{
+    graphics.draw_Image(turtle_image);
+  }
 
-      turtle_image{ "TurtleAlpha.png" }, //TODO: Magic literal
-	  bPenDown{ true }
-	{}
-
-	void draw(Graphics& graphics) const{
-	  graphics.draw_Image(turtle_image);
-	}
-
-	void forward(Units distance){
-	  point old_position = position;
-	  position.x += distance * cosD(Angle);
-      position.y += distance * sinD(Angle);
+  void forward(Units distance){
+    point old_position = position;
+    position.x += distance * cosD(Angle);
+    position.y += distance * sinD(Angle);
+  
+    if (bPenDown)
+      canvas.draw_line(old_position, position);
+  
+    turtle_image.set_position(position);
+  }
 	
-	  if (bPenDown)
-	    canvas.draw_line(old_position, position);
+  void right(Degrees angle){
+    Angle += angle;
+    Angle = fmod(Angle, 360.0);
+    turtle_image.set_rotation(Angle);
+  }
 	
-	  turtle_image.set_position(position);
-	}
-	
-	void right(Degrees angle){
-	  Angle += angle;
-	  Angle = fmod(Angle, 360.0);
-	  turtle_image.set_rotation(Angle);
-	}
-	
-	void left(Degrees angle){
-	  right(-angle);
-	}
-	void PenDown(){ 	
-	  bPenDown = true; 
-	}
-	void PenUp(){
-	  bPenDown = false;
-	}
+  void left(Degrees angle){
+    right(-angle);
+  }
+  void PenDown(){ 	
+    bPenDown = true; 
+  }
+  void PenUp(){
+    bPenDown = false;
+  }
 
 private:	
-	Canvas& canvas;
-	point position;
-	Degrees Angle;
+  Canvas& canvas;
+  point position;
+  Degrees Angle;
 
-	image turtle_image;
-	bool bPenDown;
+  image turtle_image;
+  bool bPenDown;
 };
 
 }
